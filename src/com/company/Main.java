56b25bbe655e246a0a45d3d4805a4c8aea9e143e
MbRussia <golokhvastov@gmail.com>
2019-04-17 21:45:43 +0300

    New home work city

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        cons();
-        name();
-        f();
-        rnd_array();
-        sortA();
-        createString();
 
     }
 
